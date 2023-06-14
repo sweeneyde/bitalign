1ca8ef81d7397dadcd8d5ec58fb271dbb6f56c1a
sweeneyde <sweeney.427@osu.edu>
2023-06-14 15:58:12 -0400

    Update pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import Extension, setup
 
 setup(
+    name='bitalign',
+    version='0.1',
+    author="Dennis Sweeney",
+    author_email="sweeney.427@osu.edu",
+    url="https://github.com/sweeneyde/bitalign",
+    description="Shift some bits and see how they line up best.",
     ext_modules=[
         Extension(
             name="_bitalign",
