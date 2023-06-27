9c167f6ffac261715fa89ce969c37f66c438ffb2
sweeneyde <sweeney.427@osu.edu>
2023-06-26 21:38:10 -0400

    consistent function signatures

--- src/_bitalign.h
+++ src/_bitalign.h
@@ -20,12 +20,12 @@ struct bitalign_result bitalign_impl_64msb(void *, void *, int, void *);
 
 typedef void (*implfunc_multi)(void *, void *, size_t, int, void *, struct bitalign_result *);
 
-void bitalign_multi_impl_8lsb(void *, void *, size_t, int, void *, void *);
-void bitalign_multi_impl_16lsb(void *, void *, size_t, int, void *, void *);
-void bitalign_multi_impl_32lsb(void *, void *, size_t, int, void *, void *);
-void bitalign_multi_impl_64lsb(void *, void *, size_t, int, void *, void *);
+void bitalign_multi_impl_8lsb(void *, void *, size_t, int, void *, void *, struct bitalign_result *);
+void bitalign_multi_impl_16lsb(void *, void *, size_t, int, void *, void *, struct bitalign_result *);
+void bitalign_multi_impl_32lsb(void *, void *, size_t, int, void *, void *, struct bitalign_result *);
+void bitalign_multi_impl_64lsb(void *, void *, size_t, int, void *, void *, struct bitalign_result *);
 
-void bitalign_multi_impl_8msb(void *, void *, size_t, int, void *, void *);
-void bitalign_multi_impl_16msb(void *, void *, size_t, int, void *, void *);
-void bitalign_multi_impl_32msb(void *, void *, size_t, int, void *, void *);
-void bitalign_multi_impl_64msb(void *, void *, size_t, int, void *, void *);
+void bitalign_multi_impl_8msb(void *, void *, size_t, int, void *, void *, struct bitalign_result *);
+void bitalign_multi_impl_16msb(void *, void *, size_t, int, void *, void *, struct bitalign_result *);
+void bitalign_multi_impl_32msb(void *, void *, size_t, int, void *, void *, struct bitalign_result *);
+void bitalign_multi_impl_64msb(void *, void *, size_t, int, void *, void *, struct bitalign_result *);
