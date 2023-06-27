c846fff17f289a70f083dc4f2b988f651adf1e46
sweeneyde <sweeney.427@osu.edu>
2023-06-27 03:34:32 -0400

    version 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import Extension, setup
 
 setup(
     name='bitalign',
-    version='0.3.0',
+    version='0.4.0',
     author="Dennis Sweeney",
     author_email="sweeney.427@osu.edu",
     url="https://github.com/sweeneyde/bitalign",
